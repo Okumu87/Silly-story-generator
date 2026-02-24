2ea8a60149ce1c4b306beeb30b95c996ff617a6a
okumu87 <okumu793@gmail.com>
2026-02-24 10:08:26 +0100

    Random story generator

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
-// Initialize your project here
-console.log('JavaScript file loaded successfully');
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
 
-// Add your code below
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+}
+
+// Raw text strings
+
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
+
+
+// Partial return random string function
+
+function returnRandomStoryString() {
+
+    const randomCharacter = randomValueFromArray(characters);
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
+
+    const storyText = ` It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent} Bob saw the whole thing, but was not surprised ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+
+
+  return storyText;
+}
+
+// Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+    let newStory = returnRandomStoryString();
+
+
+  if (customName.value !== "") {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 /14)} stone`;
+    const temperature = `${Math.round((94-32) * 5 / 9)} centigrade`;
+
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
